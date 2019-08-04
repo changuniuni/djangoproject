f82cd6570f5a8168d1cd41a0112325d19f4bbd6e
changchang <changchang@unist.ac.kr>
2019-08-04 16:40:56 +0900

    33rule1

--- changomok.py
+++ changomok.py
@@ -5,6 +5,9 @@ dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 dx = [0, 1, 1, 1, 0, -1, -1, -1]
 
 
+global coun
+coun=0
+
 def dfs():
     for x in range(19):
         for y in range(19):
@@ -30,7 +33,8 @@ def dfs():
 def ccheck_up(current_color,cur_xpos,cur_ypos):  #위쪽방향 탐색
     blank=0
     ccolor=0
-    for num1 in range(4):        
+    global coun
+    for num1 in range(1,4):        
 
         if current_color == 1000:
             if array[cur_ypos-num1][cur_xpos] == 2 and cur_ypos-num1 >0:
@@ -49,13 +53,15 @@ def ccheck_up(current_color,cur_xpos,cur_ypos):  #위쪽방향 탐색
                 ccolor+=1
         if blank ==2:
             break
-        if ccolor>=3 or (ccolor + blank)==4:
-            return print('이곳은 금수입니다')
+        if ccolor>1: # or ccolor + blank==3
+            coun+=1
+            break
 
 def ccheck_upright(current_color,cur_xpos,cur_ypos):  #오른쪽 위 방향 탐색
     blank=0
     ccolor=0
-    for num1 in range(4):        
+    global coun
+    for num1 in range(1,4):        
 
         if current_color == 1000:
             if array[cur_ypos-num1][cur_xpos+num1] == 2 and cur_ypos-num1 >0 and cur_xpos+num1<20:
@@ -74,13 +80,16 @@ def ccheck_upright(current_color,cur_xpos,cur_ypos):  #오른쪽 위 방향 탐�
                 ccolor+=1
         if blank ==2:
             break
-        if ccolor>=3 or (ccolor + blank)==4:
-            return print('이곳은 금수입니다')
+        if ccolor>1: # or ccolor + blank==3:
+            coun+=1
+            break
 
 def ccheck_right(current_color,cur_xpos,cur_ypos):  #오른쪽 방향 탐색
     blank=0
     ccolor=0
-    for num1 in range(4):        
+    
+    global coun
+    for num1 in range(1,4):        
 
         if current_color == 1000:
             if array[cur_ypos][cur_xpos+1] == 2 and cur_xpos+num1<20:
@@ -99,13 +108,15 @@ def ccheck_right(current_color,cur_xpos,cur_ypos):  #오른쪽 방향 탐색
                 ccolor+=1
         if blank ==2:
             break
-        if ccolor>=3 or (ccolor + blank)==4:
-            return print('이곳은 금수입니다')
+        if ccolor>1: # or ccolor + blank==3:
+            coun+=1
+            break
 
 def ccheck_downright(current_color,cur_xpos,cur_ypos):  #오른쪽 아래 방향 탐색
     blank=0
     ccolor=0
-    for num1 in range(4):        
+    global coun
+    for num1 in range(1,4):        
 
         if current_color == 1000:
             if array[cur_ypos+num1][cur_xpos+num1] == 2 and cur_ypos+num1 <20 and cur_xpos+num1<20:
@@ -124,13 +135,15 @@ def ccheck_downright(current_color,cur_xpos,cur_ypos):  #오른쪽 아래 방향
                 ccolor+=1
         if blank ==2:
             break
-        if ccolor>=3 or (ccolor + blank)==4:
-            return print('이곳은 금수입니다')
+        if ccolor>1: # or ccolor + blank==3:
+            coun+=1
+            break
 
 def ccheck_down(current_color,cur_xpos,cur_ypos):  #아래 방향 탐색
     blank=0
+    global coun
     ccolor=0
-    for num1 in range(4):        
+    for num1 in range(1,4):        
 
         if current_color == 1000:
             if array[cur_ypos+num1][cur_xpos] == 2 and cur_ypos+num1 <20 :
@@ -149,14 +162,16 @@ def ccheck_down(current_color,cur_xpos,cur_ypos):  #아래 방향 탐색
                 ccolor+=1
         if blank ==2:
             break
-        if ccolor>=3 or (ccolor + blank)==4:
-            return print('이곳은 금수입니다')
+        if ccolor>1 :# or ccolor + blank==3:
+            coun+=1
+            break
 
 
 def ccheck_downleft(current_color,cur_xpos,cur_ypos):  #왼쪽 아래 방향 탐색
     blank=0
     ccolor=0
-    for num1 in range(4):        
+    global coun
+    for num1 in range(1,4):        
 
         if current_color == 1000:
             if array[cur_ypos+num1][cur_xpos-num1] == 2 and cur_ypos+num1 <20 and cur_xpos-num1>0:
@@ -175,13 +190,15 @@ def ccheck_downleft(current_color,cur_xpos,cur_ypos):  #왼쪽 아래 방향 탐
                 ccolor+=1
         if blank ==2:
             break
-        if ccolor>=3 or (ccolor + blank)==4:
-            return print('이곳은 금수입니다')
+        if ccolor>1: # or ccolor + blank==3:
+            coun+=1
+            break
 
 def ccheck_left(current_color,cur_xpos,cur_ypos):  #왼쪽 방향 탐색
     blank=0
     ccolor=0
-    for num1 in range(4):        
+    global coun
+    for num1 in range(1,4):        
 
         if current_color == 1000:
             if array[cur_ypos][cur_xpos-num1] == 2 and cur_xpos-num1>0:
@@ -190,6 +207,7 @@ def ccheck_left(current_color,cur_xpos,cur_ypos):  #왼쪽 방향 탐색
                 blank+=1
             elif  cur_xpos-num1>0:
                 ccolor+=1
+
         
         else:
             if array[cur_ypos][cur_xpos-num1] == 1 and cur_xpos-num1>0:
@@ -200,14 +218,16 @@ def ccheck_left(current_color,cur_xpos,cur_ypos):  #왼쪽 방향 탐색
                 ccolor+=1
         if blank ==2:
             break
-        if ccolor>=3 or (ccolor + blank)==4:
-            return print('이곳은 금수입니다')
+        if ccolor>1: #or ccolor + blank==3:
+            coun+=1
+            break
 
 
 def ccheck_upleft(current_color,cur_xpos,cur_ypos):  #왼쪽위 방향 탐색
     blank=0
     ccolor=0
-    for num1 in range(4):        
+    global coun
+    for num1 in range(1,4):        
 
         if current_color == 1000:
             if array[cur_ypos-num1][cur_xpos-num1] == 2 and cur_ypos-num1>0 and cur_xpos-num1>0:
@@ -226,37 +246,45 @@ def ccheck_upleft(current_color,cur_xpos,cur_ypos):  #왼쪽위 방향 탐색
                 ccolor+=1
         if blank ==2:
             break
-        if ccolor>=3 or (ccolor + blank)==4:
-            return print('이곳은 금수입니다')
+        if ccolor>1: # or ccolor + blank==3:
+            coun+=1
+            break
 
                       
 check = False
 while check == False:
-    check1= False 
+    coun=0
     a,xpos,ypos = input().split(',')
     a=int(a)
     xpos=int(xpos)
     ypos=int(ypos)
-    if ccheck_down(a,xpos,ypos): check1=True
-    if ccheck_downleft(a,xpos,ypos): check1=True
-    if ccheck_downright(a,xpos,ypos): check1=True
-    if ccheck_left(a,xpos,ypos): check1=True
-    if ccheck_right(a,xpos,ypos): check1=True
-    if ccheck_up(a,xpos,ypos): check1=True
-    if ccheck_upleft(a,xpos,ypos): check1=True
-    if ccheck_upright(a,xpos,ypos): check1=True
+    ccheck_down(a,xpos,ypos)
+    ccheck_downleft(a,xpos,ypos)
+    ccheck_downright(a,xpos,ypos)
+    ccheck_left(a,xpos,ypos)
+    ccheck_right(a,xpos,ypos)
+    ccheck_up(a,xpos,ypos)
+    ccheck_upleft(a,xpos,ypos)
+    ccheck_upright(a,xpos,ypos)
+    print(coun)
+
+    if coun>=2:
+        print('안됨')
+        
+        continue
+       
     #a = int(input("B or W?"))
     #xpos = int(input("X position"))
     #ypos= int(input("Y position"))
-    while check1 ==False:
-        if a==1000 :
-            array[ypos][xpos]=1
-            Data=[array]
-            if dfs(): check = True
+
+    if a==1000 :
+        array[ypos][xpos]=1
+        Data=[array]
+        if dfs(): check = True
     
-        else :
-            array[ypos][xpos]=2
-            Data=[array]
-            if dfs(): check = True
+    else :
+        array[ypos][xpos]=2
+        Data=[array]
+        if dfs(): check = True
 
     
